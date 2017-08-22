77c1ef8dd918956f80a64c9c448809816e2566eb
fslic <fslic@DESKTOP-MCATCHJ>
2017-08-22 21:09:14 +0800

    new demos

--- Java_8_New_Features/src/main/java/evolution/optional/AnyPojo.java
+++ Java_8_New_Features/src/main/java/evolution/optional/AnyPojo.java
@@ -1,7 +1,23 @@
 package evolution.optional;
 
+import java.util.Optional;
+
 public class AnyPojo {
+	private String name;
 	private AnotherPojo anotherPojo;
+	private Optional<AnotherPojo> anotherPojoOptional;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Optional<AnotherPojo> getAnotherPojoOptional() {
+		return anotherPojoOptional;
+	}
+	public void setAnotherPojoOptional(Optional<AnotherPojo> anotherPojoOptional) {
+		this.anotherPojoOptional = anotherPojoOptional;
+	}
 	public AnotherPojo getAnotherPojo() {
 		return anotherPojo;
 	}
